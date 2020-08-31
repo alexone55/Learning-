7e43aece3cb163273b206df936ea056bab73bbb0
alexone55 <roprone55@gmail.com>
2020-08-31 10:17:26 +0300

    Delete Number_pi.py

--- Number_pi.py
+++
@@ -1,9 +0,0 @@
-digit = int(input("Please type count of digits: "))
-pi = 1
-
-for i in range(1, 100000000):
-    pi += ((-1) ** i) * (1 / (2 * i +1))
-
-result = pi * 4
-print(round(result, digit))
-Number_pi.py
