44529fa5b0bb38cb8cfa1bc6f20c025acbf5241b
alexone55 <roprone55@gmail.com>
2020-09-03 13:59:42 +0300

    add new files and change def names snake_case style

--- Alexey/Mortgage Calculator.py
+++ Alexey/Mortgage Calculator.py
@@ -1,42 +1,42 @@
-answer = input("Choose type of calc y/m/w/d/: ")
-P = int(input("Principal Amount (initial loan balance): "))
-i = float(input("Interest Rate: ")) / 100
-t = int(input("Count of Years: "))
-n = t * 12
-
-
 def main():
-    M = P * (i / 12 * (1 + i / 12) ** n) / ((1 + i / 12) ** n - 1)
+    answer = input("Choose type of calc y/m/w/d/: ")
+    p = int(input("Principal Amount (initial loan balance): "))
+    i = float(input("Interest Rate: ")) / 100
+    t = int(input("Count of Years: "))
+    n = t * 12
+    m = p * (i / 12 * (1 + i / 12) ** n) / ((1 + i / 12) ** n - 1)
+
     if answer.lower() == "m":
-        MonthCalculation(M)
+        month_calculation(m)
     elif answer.lower() == "y":
-        YearCalculation(M)
+        year_calculation(m)
     elif answer.lower() == "d":
-        DailyCalculation(M)
+        daily_calculation(m)
     elif answer.lower() == "w":
-        WeekCalculation(M)
+        week_calculation(m)
 
 
-def MonthCalculation(M):
-    print("Your monthly " + str(round(M, ndigits=2)))
+def month_calculation(m):
+    print("Your monthly " + str(round(m, ndigits=2)))
 
 
-def DailyCalculation(M):
-    daily = M / 30
+def daily_calculation(m):
+    daily = m / 30
     print("Your daily " + str(round(daily, ndigits=2)))
 
 
-def WeekCalculation(M):
-    week = M / 30 * 7
+def week_calculation(m):
+    week = m / 30 * 7
     print("Your weekly " + str(round(week, ndigits=2)))
 
 
-def YearCalculation(M):
-    year = M * 12
+def year_calculation(m):
+    year = m * 12
     print("Your yearly " + str(round(year, ndigits=2)))
 
+
 if __name__ == "__main__":
-     main()
+    main()
 
 
 
