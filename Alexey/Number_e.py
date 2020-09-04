a617193fff63764e5e8d924e49f90e6f6393d597
alexone55 <roprone55@gmail.com>
2020-09-04 12:23:17 +0300

    speed up

--- Alexey/Number_e.py
+++ Alexey/Number_e.py
@@ -2,15 +2,11 @@ def main():
     digit = int(input("Please type count of digits: "))
     number_e(digit)
 
+
 def number_e(digit):
-    e = 0
-
-    for i in range(1, 100000000):
-        e += ((1+(1/ i)) ** i)
-        e = e / i
-
-    result = e
-    print(round(result, digit))
+    i = 100000000
+    e = (1 + 1 / i) ** i
+    print(round(e, digit))
 
 
 if __name__ == '__main__':
