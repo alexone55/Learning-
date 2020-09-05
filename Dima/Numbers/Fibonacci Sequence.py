819ca707a394aa4551cfc81281bc1ec2ec6e6d66
Tourist88 <50734241+Tourist88@users.noreply.github.com>
2020-09-06 00:13:20 +0300

    Add fibonacci_sequence
    Add output messege into fibonacci function
    Add main function

--- Dima/Numbers/Fibonacci Sequence.py
+++ Dima/Numbers/Fibonacci Sequence.py
@@ -1,26 +1,31 @@
-print('Input number:')
-n = int(input())
-
-
-def Fib_num(n):
+def fibonacci(n):
     i = 0
-    f0 = 0
-    f1 = 1
-    fib = [0, 1]
+    previous_fib_num = 0
+    now_fib_num = 1
+    fibonacci_sequence = [0, 1]
+    ans = 'Number N - isn`t in Fibonacci sequence, here is Fibonacci sequence to N`th number: '
     if n == 0:
-        f1 = f0
-        fib = []
+        now_fib_num = previous_fib_num
+        fibonacci_sequence = []
     while i <= n:
-        fi = f0 + f1
+        fi = previous_fib_num + now_fib_num
         if fi == n:
-            fib.append(fi)
+            fibonacci_sequence.append(fi)
+            ans = 'Number N is in the Fibonacci sequence: '
             break
         else:
-            f0 = f1
-            f1 = fi
-            fib.append(fi)
+            previous_fib_num = now_fib_num
+            now_fib_num = fi
+            fibonacci_sequence.append(fi)
             i += 1
-    return fib
+    return ans, fibonacci_sequence
 
-print("List of Fib:")
-print(Fib_num(n))
+
+def main():
+    n = int(input("Enter the number 'N': "))
+    answer, fibonacci_sequence = fibonacci(n)
+    print(answer, *fibonacci_sequence)
+
+
+if __name__ == "__main__":
+    main()
