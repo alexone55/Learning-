4730f5bdc789e1395f1f21c10a0c410fa51136a2
alexone55 <roprone55@gmail.com>
2020-09-07 17:01:37 +0300

    add new file

---
+++ Alexey/distance_betwen_two_cities.py
@@ -0,0 +1,24 @@
+import math
+
+
+def main():
+    print("Coordinates of first city x,y")
+    x1 = int(input())
+    y1 = int(input())
+    print("Coordinates of second city x,y")
+    x2 = int(input())
+    y2 = int(input())
+    lets_find_city_distance(x1, y1, x2, y2)
+
+
+def lets_find_city_distance(x1, y1, x2, y2):
+    delta_x = x1 - x2
+    delta_y = y1 - y2
+    distance = math.sqrt(abs(delta_x) ** 2 +
+                         abs(delta_y) ** 2)
+
+    print(int(distance))
+
+
+if __name__ == '__main__':
+    main()
