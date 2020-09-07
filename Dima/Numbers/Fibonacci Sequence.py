ff3a2c233b9c1b626eacd936effa23fb38d584d7
Horus1223 <70372314+Horus1223@users.noreply.github.com>
2020-09-08 00:09:42 +0300

    Update Fibonacci Sequence.py

--- Dima/Numbers/Fibonacci Sequence.py
+++ Dima/Numbers/Fibonacci Sequence.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def fibonacci(n):
     i = 0
     previous_fib_num = 0
@@ -30,26 +29,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-=======
-n = int(input())
-f0 = 0
-f1 = 1
-i = 0
-
-fib = [0, 1]
-if n == 0:
-    f1 = f0
-while i <= n:
-    fi = f0 + f1
-    if fi == n:
-        fib.append(fi)
-        break
-    else:
-        f0 = f1
-        f1 = fi
-        fib.append(fi)
-        i += 1
-
-print("List of Fib:")
-print(*fib)
->>>>>>> parent of 7fb3b86... Update Fibonacci Sequence.py
