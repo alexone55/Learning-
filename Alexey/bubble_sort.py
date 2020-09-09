efba8152b221e82747c219ba01ee1906f671d42f
alexone55 <roprone55@gmail.com>
2020-09-09 19:11:45 +0300

    add file

---
+++ Alexey/bubble_sort.py
@@ -0,0 +1,16 @@
+def main():
+    array = [2, 4, 1, 5, 7, 3, 8, 9]
+    print(*array)
+    print(sort(array))
+
+
+def sort(array):
+    for id_i, i in enumerate(array):
+        for id_y, y in enumerate(array):
+            if array[id_y] > array[id_y + 1]:
+                array[id_y], array[id_y + 1] = array[id_y + 1], array[id_y]
+    return array
+
+
+if __name__ == '__main__':
+    main()
