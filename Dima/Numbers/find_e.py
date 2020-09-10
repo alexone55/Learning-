3bac313a3aba2e043e430d6df6dcdef5b09736c6
Tourist88 <50734241+Tourist88@users.noreply.github.com>
2020-09-10 14:28:36 +0300

    Add decorator
    Change factorial function to recursion

--- Dima/Numbers/find_e.py
+++ Dima/Numbers/find_e.py
@@ -1,22 +1,19 @@
-import time
-from factorial_finder import factorial_by_for
+from factorial_finder import factorial_by_recursion
+from time_decorator import timer
 
 
 def make_e(n):
     sum1 = 1
     for i in range(1, n + 1):
-        sum1 = sum1 + (1 / factorial_by_for(i))
+        sum1 = sum1 + (1 / factorial_by_recursion(i))
     result = round(sum1, n)
     return result
 
 
+@timer
 def main():
     n = int(input("Enter the n`th number to show the Euler's number digits after comma : "))
-    start_time = time.time()
     print(make_e(n))
-    end_time = time.time()
-    total_time = end_time - start_time
-    print('Time:', total_time)
 
 
 if __name__ == "__main__":
