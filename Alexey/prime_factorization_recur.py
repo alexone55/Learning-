13c64523039144119db6fbebdbbac9d276eaf38e
Your Name <you@example.com>
2020-09-10 15:21:15 +0300

    fix recursion

--- Alexey/prime_factorization_recur.py
+++ Alexey/prime_factorization_recur.py
@@ -1,19 +1,16 @@
 def main():
     n = int(input("Type number: "))
-    print(factorization_func(n))
-
-
-def factorization_func(n):
-    if n == 0:
-
-        return []
     i = 2
-    while i <= n:
-        while not n % i:
-            return [i] + factorization_func(n // i)
-    i += 1
+    print(factorization_func(n, i))
 
-    factorization_func(n)
+
+def factorization_func(i, n):
+    if i < n:
+        return []
+    if i % n == 0:
+        return [n] + factorization_func(i / n, 2)
+    return factorization_func(i, n + 1)
+
 
 
 if __name__ == '__main__':
