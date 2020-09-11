eec637d9e6ee47000d874812991f0dd30846180f
krassovki <>
2020-09-11 03:24:05 +0300

    Update change_return_program.py

--- Misha/Numbers/change_return_program.py
+++ Misha/Numbers/change_return_program.py
@@ -1,14 +1,14 @@
 def return_change(cost, summ):
-    change = round(summ-cost,2)
+    change = round(summ - cost, 2)
     print('Change: ' + str(change))
     money_array = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.05, 0.02, 0.01]
     for index in range(len(money_array)):
         counter = 0
         if change >= money_array[index]:
             while change >= money_array[index]:
-                change = round(change-money_array[index],2)
+                change = round(change - money_array[index], 2)
                 counter += 1
-        if counter>0:
+        if counter > 0:
             print(str(money_array[index]) + 'x' + str(counter) + ' ', end='')
     return 0
 
