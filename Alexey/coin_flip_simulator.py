5324c39a5d214857ef547d3fd0d0d3e346910f1c
work <work@localhost.localdomain>
2020-09-12 00:57:58 +0300

    add counter of heads and tails, add binar choise

--- Alexey/coin_flip_simulator.py
+++ Alexey/coin_flip_simulator.py
@@ -5,22 +5,30 @@ def main():
     answer = input("Flip a coin?: y/n ")
     if answer.lower() == "y":
 
-        random_number = random.randint(1, 100)
+        random_number = random.randint(0, 1)
         flip_simulator(random_number)
-        main()
     else:
         pass
 
 
-def flip_simulator(count_of_flips):
-    count_of_heads = count_of_flips / 2
-    print("Count of heads is: " + str(int(count_of_heads)))
+def flip_simulator(count_of_flips, tails=0, heads=0):
+    print("Count of heads is: " + str(int(heads)))
     if count_of_flips % 2 == 0:
-        print("Count of tails is: " + str(int(count_of_heads)))
+        print("Count of tails is: " + str(int(tails)))
         print("Tails")
+        tails += 1
     else:
-        print("Count of tails is: " + str(int(count_of_heads) + 1))
+        print("Count of tails is: " + str(int(tails)))
         print("Heads")
+        heads += 1
+    second_answer = input("Do it again? ")
+
+    if second_answer.lower() == "y":
+        random_number = random.randint(0, 1)
+        flip_simulator(random_number, tails, heads)
+    else:
+        pass
+
 
 
 if __name__ == '__main__':
