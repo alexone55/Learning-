73ef5d9a862a3f1d4cb9b9473c71be61f756768a
krassovki <>
2020-09-18 10:42:14 +0300

    Update utest_bubble_sort.py

--- Misha/OB-125_sorting/utest_bubble_sort.py
+++ Misha/OB-125_sorting/utest_bubble_sort.py
@@ -4,90 +4,56 @@ from main import bubble_sort
 
 class BubbleSortingTest(unittest.TestCase):
 
-    def test_bubble_1(self):
+    def test_bubble_sort_with_random_array(self):
         arr_to_sort = [1, 5, 1, 235, 2, 51, 5, 1, 25, 2]
         sorted_array = bubble_sort(arr_to_sort)
-        test_pass = True
-        for index in range(1, 10):
-            if sorted_array[index - 1] > sorted_array[index]:
-                test_pass = False
-                break
-        self.assertTrue(test_pass == True)
-        self.assertFalse(test_pass == False)
+        expected_result = [1, 1, 1, 2, 2, 5, 5, 25, 51, 235]
+        self.assertEqual(expected_result, sorted_array)
 
-    def test_bubble_2(self):
+    def test_bubble_with_equally_filled_array(self):
         arr_to_sort = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         sorted_array = bubble_sort(arr_to_sort)
-        test_pass = True
-        for index in range(1, 10):
-            if sorted_array[index - 1] > sorted_array[index]:
-                test_pass = False
-                break
-        self.assertTrue(test_pass == True)
-        self.assertFalse(test_pass == False)
+        expected_result = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        self.assertEqual(expected_result, sorted_array)
 
-    def test_bubble_3(self):
+    def test_bubble_with_sorted_array(self):
         arr_to_sort = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         sorted_array = bubble_sort(arr_to_sort)
-        test_pass = True
-        for index in range(1, 10):
-            if sorted_array[index - 1] > sorted_array[index]:
-                test_pass = False
-                break
-        self.assertTrue(test_pass == True)
-        self.assertFalse(test_pass == False)
+        expected_result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.assertEqual(expected_result, sorted_array)
 
-    def test_bubble_4(self):
+    def test_bubble_with_reverse_sorted_array(self):
         arr_to_sort = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
         sorted_array = bubble_sort(arr_to_sort)
-        test_pass = True
-        for index in range(1, 10):
-            if sorted_array[index - 1] > sorted_array[index]:
-                test_pass = False
-                break
-        self.assertTrue(test_pass == True)
-        self.assertFalse(test_pass == False)
+        expected_result = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        self.assertEqual(expected_result, sorted_array)
 
-    def test_bubble_5(self):
+    def test_bubble_with_one_element_array(self):
         arr_to_sort = [1]
         sorted_array = bubble_sort(arr_to_sort)
-        test_pass = True
-        # как вообще работает этот код, почему он работает, если там элемент один,а обращения
-        # по индексу как для двух???
-        for index in range(1,1):
-            if sorted_array[index - 1] > sorted_array[index]:
-                test_pass = False
-                break
-        self.assertTrue(test_pass == True)
-        self.assertFalse(test_pass == False)
+        expected_result = [1]
+        self.assertEqual(expected_result, sorted_array)
 
-    def test_bubble_6(self):
+    def test_bubble_with_wrong_string_object_in_array(self):
         arr_to_sort = [1, 5, ' ', 235, 2, 51, 5, 1, 25, 2]
-        sorted_array = bubble_sort(arr_to_sort)
-        test_pass = False
-        if sorted_array =='Error':
-            test_pass=True
-        self.assertTrue(test_pass == True)
-        self.assertFalse(test_pass == False)
+        with self.assertRaises(TypeError) as context:
+            self.assertRaises(TypeError, bubble_sort(arr_to_sort))
+        the_exception = str(context.exception)
+        self.assertEqual('TypeError', the_exception)
 
-    def test_bubble_7(self):
+    def test_bubble_with_wrong_non_type_object_in_array(self):
         arr_to_sort = [1, 5, None, 235, 2, 51, 5, 1, 25, 2]
-        sorted_array = bubble_sort(arr_to_sort)
-        test_pass = False
-        if sorted_array =='Error':
-            test_pass=True
-        self.assertTrue(test_pass == True)
-        self.assertFalse(test_pass == False)
+        with self.assertRaises(TypeError) as context:
+            self.assertRaises(TypeError, bubble_sort(arr_to_sort))
+        the_exception = str(context.exception)
+        self.assertEqual('TypeError', the_exception)
 
-    def test_bubble_8(self):
-        arr_to_sort = [None,None,None,None,None]
-        sorted_array = bubble_sort(arr_to_sort)
-        test_pass = False
-        if sorted_array =='Error':
-            test_pass=True
-        self.assertTrue(test_pass == True)
-        self.assertFalse(test_pass == False)
-
+    def test_bubble_with_array_of_none_type_objects(self):
+        arr_to_sort = [None, None, None, None, None]
+        with self.assertRaises(TypeError) as context:
+            self.assertRaises(TypeError, bubble_sort(arr_to_sort))
+        the_exception = str(context.exception)
+        self.assertEqual('TypeError', the_exception)
 
 
 if __name__ == '__main__':
