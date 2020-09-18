e90e528e9b44b0d9a5156216cee4201c38a7fd3d
krassovki <>
2020-09-18 10:42:10 +0300

    Update utest_input.py

--- Misha/OB-125_sorting/utest_input.py
+++ Misha/OB-125_sorting/utest_input.py
@@ -4,61 +4,47 @@ from main import check_input_data_format
 
 class InputTest(unittest.TestCase):
 
-    def test_input_1(self):
-        if check_input_data_format('01003012') == 'Invalid data format':
-            test_pass = True
-        else:
-            test_pass = False
-        self.assertTrue(test_pass == True)
-        self.assertFalse(test_pass == False)
+    def test_input_with_string_startet_with_zero(self):
+        with self.assertRaises(TypeError) as context:
+            self.assertRaises(TypeError, check_input_data_format('01003012'))
+        the_exception = str(context.exception)
+        self.assertEqual('Invalid data format', the_exception)
 
-    def test_input_2(self):
-        if check_input_data_format('0hb32') == 'Invalid data format':
-            test_pass = True
-        else:
-            test_pass = False
-        self.assertTrue(test_pass == True)
-        self.assertFalse(test_pass == False)
+    def test_input_with_string_with_not_only_digits(self):
+        with self.assertRaises(TypeError) as context:
+            self.assertRaises(TypeError, check_input_data_format('0hb32'))
+        the_exception = str(context.exception)
+        self.assertEqual('Invalid data format', the_exception)
 
-    def test_input_3(self):
-        if check_input_data_format('13451') == 'Invalid data format':
-            test_pass = False
-        else:
-            test_pass = True
-        self.assertTrue(test_pass == True)
-        self.assertFalse(test_pass == False)
+    def test_input_with_big_right_value(self):
+        str_value = '12345'
+        check_input_data_format(str_value)
+        int_value = int(str_value)
+        self.assertEqual(int_value, 12345)
 
-    def test_input_4(self):
-        if check_input_data_format('0') == 'Invalid data format':
-            test_pass = True
-        else:
-            test_pass = False
-        self.assertTrue(test_pass == True)
-        self.assertFalse(test_pass == False)
+    def test_input_with_zero(self):
+        with self.assertRaises(TypeError) as context:
+            self.assertRaises(TypeError, check_input_data_format('0'))
+        the_exception = str(context.exception)
+        self.assertEqual('Invalid data format', the_exception)
 
-    def test_input_5(self):
-        if check_input_data_format('7') == 'Invalid data format':
-            test_pass = False
-        else:
-            test_pass = True
-        self.assertTrue(test_pass == True)
-        self.assertFalse(test_pass == False)
+    def test_input_with_right_value(self):
+        str_value = '7'
+        check_input_data_format(str_value)
+        int_value = int(str_value)
+        self.assertEqual(int_value, 7)
 
-    def test_input_6(self):
-        if check_input_data_format('') == 'Invalid data format':
-            test_pass = True
-        else:
-            test_pass = False
-        self.assertTrue(test_pass == True)
-        self.assertFalse(test_pass == False)
+    def test_input_with_empty_string(self):
+        with self.assertRaises(TypeError) as context:
+            self.assertRaises(TypeError, check_input_data_format(''))
+        the_exception = str(context.exception)
+        self.assertEqual('Invalid data format', the_exception)
 
-    def test_input_7(self):
-        if check_input_data_format(None) == 'Invalid data format':
-            test_pass = True
-        else:
-            test_pass = False
-        self.assertTrue(test_pass == True)
-        self.assertFalse(test_pass == False)
+    def test_input_with_none_type_value(self):
+        with self.assertRaises(TypeError) as context:
+            self.assertRaises(TypeError, check_input_data_format(None))
+        the_exception = str(context.exception)
+        self.assertEqual('Invalid data format', the_exception)
 
 
 if __name__ == '__main__':
