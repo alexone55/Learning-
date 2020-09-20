9ba1565d4114da623decb2487d365cfb78f514df
krassovki <>
2020-09-20 10:16:56 +0300

    Update utest_input.py

--- Misha/OB-125_sorting/utest_input.py
+++ Misha/OB-125_sorting/utest_input.py
@@ -7,14 +7,14 @@ class InputTest(unittest.TestCase):
     def test_input_with_string_startet_with_zero(self):
         with self.assertRaises(TypeError) as context:
             self.assertRaises(TypeError, check_input_data_format('01003012'))
-        the_exception = str(context.exception)
-        self.assertEqual('Invalid data format', the_exception)
+        exception_message = str(context.exception)
+        self.assertEqual('Invalid data format', exception_message)
 
     def test_input_with_string_with_not_only_digits(self):
         with self.assertRaises(TypeError) as context:
             self.assertRaises(TypeError, check_input_data_format('0hb32'))
-        the_exception = str(context.exception)
-        self.assertEqual('Invalid data format', the_exception)
+        exception_message = str(context.exception)
+        self.assertEqual('Invalid data format', exception_message)
 
     def test_input_with_big_right_value(self):
         str_value = '12345'
@@ -25,8 +25,8 @@ class InputTest(unittest.TestCase):
     def test_input_with_zero(self):
         with self.assertRaises(TypeError) as context:
             self.assertRaises(TypeError, check_input_data_format('0'))
-        the_exception = str(context.exception)
-        self.assertEqual('Invalid data format', the_exception)
+        exception_message = str(context.exception)
+        self.assertEqual('Invalid data format', exception_message)
 
     def test_input_with_right_value(self):
         str_value = '7'
@@ -37,14 +37,14 @@ class InputTest(unittest.TestCase):
     def test_input_with_empty_string(self):
         with self.assertRaises(TypeError) as context:
             self.assertRaises(TypeError, check_input_data_format(''))
-        the_exception = str(context.exception)
-        self.assertEqual('Invalid data format', the_exception)
+        exception_message = str(context.exception)
+        self.assertEqual('Invalid data format', exception_message)
 
     def test_input_with_none_type_value(self):
         with self.assertRaises(TypeError) as context:
             self.assertRaises(TypeError, check_input_data_format(None))
-        the_exception = str(context.exception)
-        self.assertEqual('Invalid data format', the_exception)
+        exception_message = str(context.exception)
+        self.assertEqual('Invalid data format', exception_message)
 
 
 if __name__ == '__main__':
