34131213560145b039f7b706e85b196db028b5f7
krassovki <>
2020-09-20 21:10:52 +0300

    Create creating_route_matrix_test.py

---
+++ Misha/OB-128_closest_pair_problem/creating_route_matrix_test.py
@@ -0,0 +1,35 @@
+import unittest
+from closest_pair_problem import create_route_matrix
+
+
+class CreatingRouteMatrixTest(unittest.TestCase):
+
+    def test_create_route_matrix_with_right_parameters(self):
+        route_matrix = create_route_matrix([[1, 1], [1, 2]])
+        expected_result = [[0, 1],
+                           [0, 0]]  # counted by self
+        self.assertEqual(expected_result, route_matrix)
+
+    def test_create_route_matrix_with_right_parameters_and_minimal_amount_of_dots(self):
+        route_matrix = create_route_matrix([[1, 1], [1, 2], [2, 2]])
+
+        expected_result = [[0, 1, 2 ** (1 / 2)],
+                           [0, 0, 1],
+                           [0, 0, 0]]  # counted by self
+        self.assertEqual(expected_result, route_matrix)
+
+    def test_create_route_matrix_with_non_type_object_in_matrix(self):
+        with self.assertRaises(TypeError) as context:
+            self.assertRaises(TypeError, create_route_matrix([[1, None], [1, 2], [2, 2]]))
+        exception_message = str(context.exception)
+        self.assertEqual('TypeError', exception_message)
+
+    def test_create_route_matrix_with_string_object_in_matrix(self):
+        with self.assertRaises(TypeError) as context:
+            self.assertRaises(TypeError, create_route_matrix([[1, 'sc'], [1, 2], [2, 2]]))
+        exception_message = str(context.exception)
+        self.assertEqual('TypeError', exception_message)
+
+
+if __name__ == '__main__':
+    unittest.main()
