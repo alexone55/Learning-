a26654263b38a06c474e39066e7c33c3f786f534
krassovki <>
2020-09-20 10:32:02 +0300

    Update utest_create_route_matrix.py

--- Misha/OB-128_closest_pair_problem/utest_create_route_matrix.py
+++ Misha/OB-128_closest_pair_problem/utest_create_route_matrix.py
@@ -21,14 +21,14 @@ class CreatingMatrixTest(unittest.TestCase):
     def test_create_route_matrix_with_non_type_object_in_matrix(self):
         with self.assertRaises(TypeError) as context:
             self.assertRaises(TypeError, create_route_matrix([[1, None], [1, 2], [2, 2]]))
-        the_exception = str(context.exception)
-        self.assertEqual('TypeError', the_exception)
+        exception_message = str(context.exception)
+        self.assertEqual('TypeError', exception_message)
 
     def test_create_route_matrix_with_string_object_in_matrix(self):
         with self.assertRaises(TypeError) as context:
             self.assertRaises(TypeError, create_route_matrix([[1, 'sc'], [1, 2], [2, 2]]))
-        the_exception = str(context.exception)
-        self.assertEqual('TypeError', the_exception)
+        exception_message = str(context.exception)
+        self.assertEqual('TypeError', exception_message)
 
 
 if __name__ == '__main__':
