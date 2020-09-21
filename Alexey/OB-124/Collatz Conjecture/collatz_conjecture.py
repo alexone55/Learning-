c57c638fe88e08b91fab9c9175472e2cb8bda27d
Your Name <you@example.com>
2020-09-21 07:29:16 -0400

    make git beautiful again

---
+++ Alexey/OB-124/Collatz Conjecture/collatz_conjecture.py
@@ -0,0 +1,25 @@
+def main():
+    try:
+        num = int(input("Enter number: "))
+    except ValueError:
+        num = 0
+    print(conjecture(num))
+
+
+def conjecture(num):
+    conjecture_list = []
+    while num != 1:
+        if num == 0:
+            return 0
+        elif num % 2 == 0:
+            num = num / 2
+            conjecture_list.append(int(num))
+        elif num % 2 != 0:
+            num = num * 3 + 1
+            conjecture_list.append(int(num))
+
+    return conjecture_list
+
+
+if __name__ == '__main__':
+    main()
