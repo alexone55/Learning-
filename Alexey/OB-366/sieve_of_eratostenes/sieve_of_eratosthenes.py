4ace073ae3dd64c129c0f409af14a1a2d25ef93a
Your Name <you@example.com>
2020-09-21 16:25:05 +0300

    add more tests

---
+++ Alexey/OB-366/sieve_of_eratostenes/sieve_of_eratosthenes.py
@@ -0,0 +1,27 @@
+def main():
+    try:
+        number = int(input("Enter number: "))
+    except ValueError:
+        number = 0
+    print(eratosthenes(number))
+
+
+def eratosthenes(number):
+    list_of_numbers = []
+    try:
+        for i in range(2, number + 1):
+            list_of_numbers.append(i)
+        for i in list_of_numbers:
+            for elem in list_of_numbers:
+                if elem % i == 0 and elem != i:
+                    list_of_numbers.remove(elem)
+        return list_of_numbers
+
+    except TypeError:
+        return list_of_numbers
+
+
+
+
+if __name__ == '__main__':
+    main()
