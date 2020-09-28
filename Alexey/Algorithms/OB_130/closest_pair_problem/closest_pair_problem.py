ac90551af83fcc14efba5c9a57e6b6b96337c527
Your Name <you@example.com>
2020-09-28 17:09:08 +0300

    Add test file, and add more functionality to cpp file

--- Alexey/Algorithms/OB_130/closest_pair_problem/closest_pair_problem.py
+++ Alexey/Algorithms/OB_130/closest_pair_problem/closest_pair_problem.py
@@ -2,16 +2,29 @@ import math
 
 
 def main():
-    dots = [(1, 2), (3, 6), (3, 12)]
-    min_distance = { }
+    dots = {'A': (16, 10), 'B': (136, 19), 'C': (6, 83), 'D': (8, 36)}
 
-    for dot in dots:
-        for another_dot in dots:
-            if dot != another_dot:
-                lets_find_dots_distance(dot[0], dot[1],
-                                                        another_dot[0],
-                                                        another_dot[1]))
-    print(min(min_distance))
+    print(lets_find_minimum_distance(dots))
+
+
+def lets_find_minimum_distance(dots):
+    min_distance = {}
+    count_of_dots = len(dots)
+    if isinstance(dots, dict):
+        if count_of_dots > 1:
+            for dot, dot_keys in zip(dots.values(), dots.keys()):
+                for another_dot, another_dot_keys in zip(dots.values(), dots.keys()):
+                    if dot != another_dot:
+                        min_distance[str(dot_keys) + ":"
+                                     + str(another_dot_keys)] = lets_find_dots_distance(dot[0], dot[1],
+                                                                                        another_dot[0],
+                                                                                        another_dot[1])
+            return min(min_distance, key=min_distance.get), min(min_distance.values())
+        else:
+            alert = "You have just one dot"
+            return alert
+    else:
+        raise TypeError('This is not dict')
 
 
 def lets_find_dots_distance(x1, y1, x2, y2):
@@ -19,8 +32,6 @@ def lets_find_dots_distance(x1, y1, x2, y2):
     delta_y = y1 - y2
     distance = math.sqrt(abs(delta_x) ** 2 +
                          abs(delta_y) ** 2)
-    # print(distance)
-
     return distance
 
 
