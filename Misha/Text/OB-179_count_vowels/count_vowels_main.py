4e05042fb61af8908ef649165ca5949af6839a14
krassovki <krassovki@protonmail.com>
2020-10-01 19:21:06 +0300

    OB-179_count_vowels
    - Added count_vowels_main
    - Added count_vowels_test

---
+++ Misha/Text/OB-179_count_vowels/count_vowels_main.py
@@ -0,0 +1,20 @@
+def count_vowels(text):
+    if type(text) != str:
+        raise TypeError('TypeError')
+    else:
+        text = text.lower()
+        counted_vowels = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0}
+        for letter in text:
+            if letter in counted_vowels:
+                counted_vowels[letter] += 1
+        return counted_vowels
+
+
+def main():
+    text = str(input('Enter some text to count vowels in them: '))
+    vowels = count_vowels(text)
+    print(vowels)
+
+
+if __name__ == "__main__":
+    main()
