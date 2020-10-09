caebaa4e627c1e7e8fea67dbef16252561ab70ff
alexone55 <you@example.com>
2020-10-09 19:45:01 +0300

    add each vowel count and int message

--- Alexey/Text/count_vowels/main/count_vowels.py
+++ Alexey/Text/count_vowels/main/count_vowels.py
@@ -4,11 +4,25 @@ def main():
 
 
 def vowel_counter(sentence):
-    count = 0
-    for liter in sentence:
-        if liter.lower() in "aeiou":
-            count += 1
-    return count
+    if isinstance(sentence, str):
+        char_a = sentence.count('a')
+        char_e = sentence.count('e')
+        char_i = sentence.count('i')
+        char_o = sentence.count('o')
+        char_u = sentence.count('u')
+        count = 0
+        for char in sentence:
+            if char.lower() in "aeiou":
+                count += 1
+        answer = str('Char A is: ' + str(char_a) +
+                     '| Char E is : ' + str(char_e) +
+                     '| Char I is : ' + str(char_i) +
+                     '| Char O is : ' + str(char_o) +
+                     '| Char U is : ' + str(char_u) +
+                     '| Total: ' + str(count))
+        return answer
+    else:
+        return 'There is no vowels here'
 
 
 if __name__ == '__main__':
--- Alexey/Text/count_vowels/tests/test_count_of_vowels.py
+++ Alexey/Text/count_vowels/tests/test_count_of_vowels.py
@@ -4,18 +4,17 @@ from Alexey.Text.count_vowels.main.count_vowels import vowel_counter
 
 class TestCountVowels(unittest.TestCase):
     def test_send_normal_sentence_and_expect_count_vowels(self):
-        expect_set = 21
+        expect_set = 'Char A is: 9| Char E is : 6| Char I is : 2| Char O is : 4| Char U is : 0| Total: 21'
         actual_set = vowel_counter('Captain Cold and his hotheaded partner Heat Wave plot to steal a')
         self.assertEqual(expect_set, actual_set)
 
     def test_send_sentence_with_symbols_and_numbers_and_expect_normal_vowels_count(self):
-        expect_set = 4
+        expect_set = 'Char A is: 1| Char E is : 2| Char I is : 0| Char O is : 0| Char U is : 1| Total: 4'
         actual_set = vowel_counter('Blade Runner - 2049')
         self.assertEqual(expect_set, actual_set)
 
-    def test_send_wrong_type_of_data_and_expect_error(self):
-        with self.assertRaises(TypeError) as context:
-            self.assertRaises(TypeError, vowel_counter(12))
-        exception_message = str(context.exception)
-        self.assertEqual("'int' object is not iterable", exception_message)
+    def test_send_numeric_type_of_data_and_expect_no_vowel_message(self):
+        expect_set = 'There is no vowels here'
+        actual_set = vowel_counter(12)
+        self.assertEqual(expect_set, actual_set)
 
