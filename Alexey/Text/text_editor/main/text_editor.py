fcb4e53a03349aab847ca82fbbcb154d9dafeb48
alexone55 <you@example.com>
2020-10-09 19:01:21 +0300

    fix int writing and tests to it

--- Alexey/Text/text_editor/main/text_editor.py
+++ Alexey/Text/text_editor/main/text_editor.py
@@ -1,28 +1,34 @@
 def main():
-    command = str(input('Enter command: r/w/a or help '))
-    some_text = str(input('Enter some text\n'))
-    if command == 'w':
-        write_file(12)
-    elif command == 'a':
-        edit_file(some_text)
-    elif command == 'r':
-        print(check_file())
-    elif command == 'help':
-        print('r - read')
-        print('w - write')
-        print('a - append')
-    else:
-        print('Unknown function!')
+    command = ''
+    ask_for_exit = ''
+    while ask_for_exit != '\q':
+        command = str(input('Enter command: r/w/a or help '))
+        if command == 'w':
+            some_text = str(input('Enter some text\n'))
+            write_file(some_text)
+        elif command == 'a':
+            some_text = str(input('Enter some text\n'))
+            edit_file(some_text)
+        elif command == 'r':
+            print(check_file())
+
+        elif command == 'help':
+            print('r - read')
+            print('w - write')
+            print('a - append')
+        else:
+            print('Unknown function!')
+        ask_for_exit = input('Enter \q to exit ')
 
 
 def edit_file(some_text):
     with open('new_text.txt', 'a') as file:
-        file.write(some_text)
+        file.write(str(some_text))
 
 
 def write_file(some_text):
     with open('new_text.txt', 'w') as file:
-        file.write(some_text)
+        file.write(str(some_text))
 
 
 def check_file():
--- Alexey/Text/text_editor/tests/test_text_editor.py
+++ Alexey/Text/text_editor/tests/test_text_editor.py
@@ -3,6 +3,7 @@ from Alexey.Text.text_editor.main.text_editor import *
 
 
 class TestTextEditor(unittest.TestCase):
+
     def test_write_normal_text_and_expect_file_with_text(self):
         expect_set = "Awesome!"
         some_text = "Awesome!"
@@ -10,19 +11,19 @@ class TestTextEditor(unittest.TestCase):
         self.assertEqual(expect_set, check_file())
 
     def test_append_normal_text_and_expect_file_with_appended_text(self):
-        expect_set = "Awesome!Awesome!"
+        expect_set = "Awesome!"
         some_text = "Awesome!"
         edit_file(some_text)
         self.assertEqual(expect_set, check_file())
 
-    def test_send_wrong_type_of_data_to_write_function_and_expect_type_error(self):
-        with self.assertRaises(TypeError) as context:
-            self.assertRaises(TypeError, write_file(12))
-        exception_message = str(context.exception)
-        self.assertEqual('write() argument must be str, not int', exception_message)
+    def test_send_int_to_write_function_and_expect_file_with_text(self):
+        expect_set = "12"
+        some_text = 12
+        write_file(some_text)
+        self.assertEqual(expect_set, check_file())
 
-    def test_send_wrong_type_of_data_to_edit_function_and_expect_type_error(self):
-        with self.assertRaises(TypeError) as context:
-            self.assertRaises(TypeError, edit_file(12))
-        exception_message = str(context.exception)
-        self.assertEqual('write() argument must be str, not int', exception_message)
+    def test_send_int_to_write_function_and_expect_file_with_appended_text(self):
+        expect_set = "Awesome!12"
+        some_text = 12
+        edit_file(some_text)
+        self.assertEqual(expect_set, check_file())
