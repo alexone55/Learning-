fecb8fd93bcb346966e21f018add2807f3b85a5d
krassovki <krassovki@protonmail.com>
2020-11-02 21:52:12 +0200

    zip_creator_2
    - Added test for main

--- Misha/Files/zip_creator/zip_test.py
+++ Misha/Files/zip_creator/zip_test.py
@@ -1,6 +1,7 @@
 import unittest
 import os
-from zip_main import check_set_of_paths, create_archive
+from unittest.mock import patch
+from zip_main import check_set_of_paths, create_archive, main
 import zipfile
 
 
@@ -9,9 +10,15 @@ class ZipTest(unittest.TestCase):
     def test_check_set_of_paths_with_wrong_paths(self):
         set_of_paths = ['path/to/file.1', 'path.txt', 'file.1']
         with self.assertRaises(FileNotFoundError) as context:
-            self.assertRaises(FileNotFoundError, check_set_of_paths(set_of_paths))
+            check_set_of_paths(set_of_paths)
         exception_message = str(context.exception)
         self.assertEqual('List of paths is empty', exception_message)
+
+    def test_check_set_of_paths_with_right_paths(self):
+        set_of_paths = ['zip_test.py', 'zip_main.py', 'file.1']
+        actual_set = check_set_of_paths(set_of_paths)
+        expected_set = ['zip_test.py', 'zip_main.py']
+        self.assertEqual(actual_set, expected_set)
 
     def test_create_archive_with_right_set_of_paths(self):
         set_of_paths = ['zip_main.py', 'zip_test.py']
@@ -27,7 +34,14 @@ class ZipTest(unittest.TestCase):
     def test_create_archive_with_wrong_paths(self):
         set_of_paths = ['path/to/file.1', 'path.txt', 'file.1']
         with self.assertRaises(FileNotFoundError) as context:
-            self.assertRaises(FileNotFoundError, create_archive(set_of_paths, 'wrong_archive.zip'))
+            create_archive(set_of_paths, 'wrong_archive.zip')
+        exception_message = str(context.exception)
+        self.assertEqual('List of paths is empty', exception_message)
+
+    @patch('builtins.input', side_effect=('path/to/file.1', 'path.txt', 'file.1', ''))
+    def test_main(self, mock_input):
+        with self.assertRaises(FileNotFoundError) as context:
+            main()
         exception_message = str(context.exception)
         self.assertEqual('List of paths is empty', exception_message)
 
