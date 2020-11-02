ea543faae208e013a43fe09d2ff8129ef83baab1
krassovki <krassovki@protonmail.com>
2020-11-02 21:37:03 +0200

    mp3_tagger_2
    - Added input test for main

--- Misha/Files/mp3_tagger/tagger_main.py
+++ Misha/Files/mp3_tagger/tagger_main.py
@@ -3,14 +3,11 @@ import eyed3
 
 def input_mp3_file_name(mp3_file_name):
     file_split = str(mp3_file_name).split('.')
-    if len(file_split) == 1 or file_split[len(file_split)-1] != 'mp3':
+    if len(file_split) == 1 or file_split[len(file_split) - 1] != 'mp3':
         raise TypeError('Invalid file type')
     else:
-        try:
-            audiofile = eyed3.load(mp3_file_name)
-            return audiofile
-        except OSError:
-            raise OSError('file not found')
+        audiofile = eyed3.load(mp3_file_name)
+        return audiofile
 
 
 def get_tag(audiofile):
@@ -22,24 +19,21 @@ def get_tag(audiofile):
     return tags_dict
 
 
-def set_tag(audiofile, tags_dict):
-    try:
-        audiofile.tag.artist = tags_dict['artist']
-        audiofile.tag.album = tags_dict['album']
-        audiofile.tag.album_artist = tags_dict['album artist']
-        audiofile.tag.title = tags_dict['title']
-        audiofile.tag.track_num = tags_dict['track num']
-        audiofile.tag.save()
-    except KeyError:
-        raise KeyError
+def set_tag(audiofile, tag_dict):
+    audiofile.tag.artist = tag_dict['artist']
+    audiofile.tag.album = tag_dict['album']
+    audiofile.tag.album_artist = tag_dict['album artist']
+    audiofile.tag.title = tag_dict['title']
+    audiofile.tag.track_num = tag_dict['track num']
+    audiofile.tag.save()
 
 
 def main():
     mp3_file_name = str(input('Enter path or mp3-file name: '))
     audiofile = input_mp3_file_name(mp3_file_name)
-    audiofile_tags = get_tag(audiofile)
+    audiofile_tag = get_tag(audiofile)
+    return 0
 
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
--- Misha/Files/mp3_tagger/tagger_tests.py
+++ Misha/Files/mp3_tagger/tagger_tests.py
@@ -1,33 +1,17 @@
 import unittest
+from unittest.mock import patch
+from unittest.mock import MagicMock
 import eyed3
-from tagger_main import input_mp3_file_name, set_tag, get_tag
+from tagger_main import input_mp3_file_name, set_tag, get_tag, main
 
 
 class TaggerTest(unittest.TestCase):
 
     def test_input_mp3_file_name_with_wrong_path(self):
         with self.assertRaises(TypeError) as context:
-            self.assertRaises(TypeError, input_mp3_file_name('path/to/file/'))
+            input_mp3_file_name('path/to/file/')
         exception_message = str(context.exception)
         self.assertEqual('Invalid file type', exception_message)
-
-    def test_input_mp3_file_name_with_wrong_filename(self):
-        with self.assertRaises(OSError) as context:
-            self.assertRaises(OSError, input_mp3_file_name('file.mp3'))
-        exception_message = str(context.exception)
-        self.assertEqual('file not found', exception_message)
-
-    def test_tag_setter_with_wrong_dict(self):
-        audiofile = eyed3.load('Распродажа_на_AliExpress_Максим_Галкин.mp3')
-        tags_dict = {'artist': 'artist_name',
-                     'album': 'album_name',
-                     'album artist': 'alb_artist_name',
-                     'title': 'audio_title',
-                     'track number': (1, 1)}
-        with self.assertRaises(KeyError) as context:
-            self.assertRaises(KeyError, set_tag(audiofile, tags_dict))
-        exception_message = str(context.exception)
-        self.assertEqual('', exception_message)
 
     def test_tag_setter_with_right_dict(self):
         audiofile = eyed3.load('Распродажа_на_AliExpress_Максим_Галкин.mp3')
@@ -39,6 +23,30 @@ class TaggerTest(unittest.TestCase):
         set_tag(audiofile, tags_dict)
         audiofile_tags = get_tag(audiofile)
         self.assertEqual(tags_dict, audiofile_tags)
+
+    def test_tag_setter_with_right_dict_and_big_strings(self):
+        audiofile = eyed3.load('Распродажа_на_AliExpress_Максим_Галкин.mp3')
+        tags_dict = {'artist': 'artist_name'*228,
+                     'album': 'album_name'*1488,
+                     'album artist': 'alb_artist_name'*88,
+                     'title': 'audio_title'*9524,
+                     'track num': (1, 1)}
+        set_tag(audiofile, tags_dict)
+        audiofile_tags = get_tag(audiofile)
+        self.assertEqual(tags_dict, audiofile_tags)
+
+    @patch('builtins.input', side_effect=['qwerty'])  # called all string only when in [], otherwise called first letter
+    def test_main_1(self, mock_input):
+        with self.assertRaises(TypeError) as context:
+            main()
+        exception_message = str(context.exception)
+        self.assertEqual('Invalid file type', exception_message)
+
+    @patch('builtins.input', side_effect=['Распродажа_на_AliExpress_Максим_Галкин.mp3'])
+    def test_main_2(self, mock_input):
+        expected_value = 0
+        actual_value = main()
+        self.assertEqual(expected_value, actual_value)
 
 
 if __name__ == '__main__':
