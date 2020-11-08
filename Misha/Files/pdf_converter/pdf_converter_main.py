f80c8d8ac1bf6eb6472726fe86369149bb5f86ad
krassovki <70575500+krassovki@users.noreply.github.com>
2020-11-09 01:54:53 +0200

    PDF_converter (#74)
    * PDF_converter
    
    - Added main
    - Added tests
    
    * PDF-converter 2nd attempt
    
    - Added test for long string
    - Changed way to write info in pdf file
    
    * pdf_converter_3
    
    - Deleted useless raise
    - Deleted useless test
    - Asserts in tests should be as they are
    
    * pdf_converter
    
    - Corrected all asserts
    
    * pdf_converter
    
    - AssertEqual() changed to AssertTrue

---
+++ Misha/Files/pdf_converter/pdf_converter_main.py
@@ -0,0 +1,49 @@
+from fpdf import FPDF
+
+
+def read_from_file(path):
+    f = open(str(path), 'r', encoding='latin-1')
+    text = ''
+    for line in f:
+        text += line
+    f.close()
+    print(text)
+    return text
+
+
+def change_format_to_pdf(path):
+    dot_index = 0
+    for symbol_index in range(len(path)-1, 0, -1):
+        if path[symbol_index] == '.':
+            dot_index = symbol_index
+            break
+    if dot_index == 0:
+        raise TypeError('Not path given')
+    else:
+        path = path[0:dot_index] + '.pdf'
+        return path
+
+
+def convert_to_pdf(text, path):
+    path = change_format_to_pdf(path)
+    pdf = FPDF(orientation='P', format='A4')
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    pdf.set_auto_page_break(auto=True)
+    pdf.multi_cell(w=200, h=10, txt=text)
+    pdf.output(path)
+
+
+def main():
+    path = str(input('Enter file name or path to file: '))
+    # here are available files to test input
+    #test_file.html
+    #test_file.ibc
+    #test_file.txt
+    text = read_from_file(path)
+    path = change_format_to_pdf(path)
+    convert_to_pdf(text, path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ Misha/Files/pdf_converter/pdf_converter_tests.py
@@ -0,0 +1,29 @@
+import unittest
+import os
+from pdf_converter_main import read_from_file, change_format_to_pdf, convert_to_pdf
+
+
+class PDFConverterTest(unittest.TestCase):
+
+    def test_read_from_file_with_right_path(self):
+        text = read_from_file('test_file.ibc')
+        expected_value = 'text128281488'
+        self.assertEqual(expected_value, text)
+
+    def test_change_format_to_pdf_with_wrong_path(self):
+        with self.assertRaises(TypeError) as context:
+            change_format_to_pdf('path/to/file/')
+        exception_message = str(context.exception)
+        self.assertEqual('Not path given', exception_message)
+
+    def test_change_format_to_pdf_with_right_path(self):
+        path = change_format_to_pdf('path.txt')
+        expected_value = 'path.pdf'
+        self.assertEqual(expected_value, path)
+
+    def test_create_pdf_with_long_string_without_blanks(self):
+        text = read_from_file('longstr.txt')
+        path_to_pdf = change_format_to_pdf('longstr.txt')
+        convert_to_pdf(text, path_to_pdf)
+        actual_answer = os.path.exists(path_to_pdf)
+        self.assertTrue(actual_answer)
